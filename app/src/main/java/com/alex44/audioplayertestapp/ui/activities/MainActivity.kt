6c9ddbe6e15469d9e571b45f18f9d3943113482c
Alexandr44 <alexandrgluhov@mail.ru>
2020-01-25 01:31:10 +0500

    MainActivity и App

---
+++ app/src/main/java/com/alex44/audioplayertestapp/ui/activities/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.alex44.audioplayertestapp.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.alex44.audioplayertestapp.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
