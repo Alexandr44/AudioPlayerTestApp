38eadad099cd7fd2df48f978d4ae2da119071b48
Alexandr44 <alexandrgluhov@mail.ru>
2020-01-25 07:20:05 +0500

    Дописал модуль даггер RepoModule

--- app/src/main/java/com/alex44/audioplayertestapp/di/modules/RepoModule.kt
+++ app/src/main/java/com/alex44/audioplayertestapp/di/modules/RepoModule.kt
@@ -1,8 +1,16 @@
 package com.alex44.audioplayertestapp.di.modules
 
+import com.alex44.audioplayertestapp.common.interfaces.INetworkStatus
+import com.alex44.audioplayertestapp.model.api.IDataSource
+import com.alex44.audioplayertestapp.model.repo.DataRepo
+import com.alex44.audioplayertestapp.model.repo.IDataRepo
 import dagger.Module
+import dagger.Provides
 
 @Module(includes = [ApiModule::class, NetworkModule::class])
 class RepoModule {
+
+    @Provides
+    fun repo(dataSource : IDataSource, networkStatus : INetworkStatus) : IDataRepo = DataRepo(dataSource, networkStatus)
 
 }
