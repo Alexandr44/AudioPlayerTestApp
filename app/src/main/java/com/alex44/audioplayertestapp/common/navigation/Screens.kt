580908b7a67a3fe9c836eb19fd634d59d8ee2147
Alexandr44 <alexandrgluhov@mail.ru>
2020-01-25 07:23:33 +0500

    Добавлен экран для навигации

--- app/src/main/java/com/alex44/audioplayertestapp/common/navigation/Screens.kt
+++ app/src/main/java/com/alex44/audioplayertestapp/common/navigation/Screens.kt
@@ -1,5 +1,13 @@
 package com.alex44.audioplayertestapp.common.navigation
 
+import androidx.fragment.app.Fragment
+import com.alex44.audioplayertestapp.ui.activities.HomeFragment
+import ru.terrakok.cicerone.android.support.SupportAppScreen
+
 class Screens {
+
+    class HomeScreen : SupportAppScreen() {
+        override fun getFragment(): Fragment = HomeFragment.newInstance();
+    }
 
 }
