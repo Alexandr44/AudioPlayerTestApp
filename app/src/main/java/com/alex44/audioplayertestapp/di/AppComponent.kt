e3c96de45d0780fed4ffdae413b6b7b88e03602c
Alexandr44 <alexandrgluhov@mail.ru>
2020-01-25 17:28:38 +0500

    Методы DI

--- app/src/main/java/com/alex44/audioplayertestapp/di/AppComponent.kt
+++ app/src/main/java/com/alex44/audioplayertestapp/di/AppComponent.kt
@@ -4,10 +4,12 @@ import com.alex44.audioplayertestapp.di.modules.AppModule
 import com.alex44.audioplayertestapp.di.modules.CiceroneModule
 import com.alex44.audioplayertestapp.di.modules.ImageModule
 import com.alex44.audioplayertestapp.di.modules.RepoModule
+import com.alex44.audioplayertestapp.presenters.DetailPresenter
 import com.alex44.audioplayertestapp.presenters.HomePresenter
 import com.alex44.audioplayertestapp.presenters.MainPresenter
 import com.alex44.audioplayertestapp.ui.activities.MainActivity
 import com.alex44.audioplayertestapp.ui.adapters.HomeRVAdapter
+import com.alex44.audioplayertestapp.ui.fragments.DetailFragment
 import dagger.Component
 import javax.inject.Singleton
 
@@ -19,5 +21,7 @@ interface AppComponent {
     fun inject(mainActivity: MainPresenter)
     fun inject(presenter: HomePresenter)
     fun inject(adapter: HomeRVAdapter)
+    fun inject(detailFragment: DetailFragment)
+    fun inject(presenter: DetailPresenter)
 
 }
